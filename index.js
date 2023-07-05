a40c463af317e48a3a753cdc36cc3bfc8d5721be
gift kimani <gift.kimani@students.moringaschool.com>
2023-07-05 16:11:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
+word = word.toLowerCase();
+const reversedWord = word.split("").reverse().join("");
+return word === reversedWord;
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+  initialize a function called isPalindrome
+  convert the words to lowercase,split,reverse and join the elements.
+  return the comparison of the original word and the reversed word.
 */
 
 /*
   Add written explanation of your solution here
+  first convert the word to lowercase 
+  split the words using.split("")
+  reverse the words using reverse()
+  join the words using.join("")
+  compare both the original and reversed words.
 */
 
 // You can run `node index.js` to view these console logs
